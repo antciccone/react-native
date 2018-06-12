9c928d4e95f3e941a23a020d50328d73ff6270d5
antciccone <aciccone2014@gmail.com>
2018-06-12 17:33:56 -0400

    add tech stack app

--- tech_stack/src/App.js
+++ tech_stack/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import { View } from 'react-native';
+import reducers from './reducers'
+import Header from './components/common/Header'
+import LibraryList from './components/LibraryList'
 
 const App = () => {
   return (
-    <Provider store={createStore}>
-      <View />
+     // Provider can only have one child components. facilitates info between react and redux
+    <Provider store={createStore(reducers)}>
+      <View style={{flex: 1}}>
+        <Header headerText="Tech Stack"/>
+        <LibraryList/>
+      </View>
     </Provider>
   )
 }
